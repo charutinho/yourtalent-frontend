9318d98c034225774c2dafdc97e2ad5ceaa65871
almeidalucas78 <lucas-silva-dealmeida123@hotmail.com>
2019-11-21 17:33:09 -0300

    Estlização da escolha de esportes

--- src/pages/EscolherCategoriaBanco/index.js
+++ src/pages/EscolherCategoriaBanco/index.js
@@ -1,10 +1,11 @@
-       import React, { Component, useEffect, useState, useCallback } from 'react';
+import React, { Component, useEffect, useState, useCallback } from 'react';
 import {
     View,
     Text,
     FlatList,
     TouchableOpacity,
-    StatusBar
+    StatusBar,
+    ImageBackground
 } from 'react-native';
 import styles from './styles';
 import AsyncStorage from '@react-native-community/async-storage';
@@ -14,7 +15,9 @@ export default class Categorias extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            listData: ''
+            listData: '',
+            esporte: false,
+            esport: false
         }
     }
 
@@ -25,12 +28,12 @@ export default class Categorias extends Component {
     async componentDidMount() {
         const { navigation } = this.props;
         const option = navigation.getParam('esporte');
-        const ip = await AsyncStorage.getItem('@Ip:ip');
-        const response = await fetch(`http://${ip}:3000/esportes/listar${option}s`);
-        esportes = await response.json();
-        this.setState({
-            listData: esportes
-        })
+        if (option == 'Esporte') {
+            await this.setState({ esporte: true })
+        } else {
+            await this.setState({ esport: true })
+        }
+
     }
 
     navegar = async (esporte) => {
@@ -42,6 +45,8 @@ export default class Categorias extends Component {
     }
 
     render() {
+        const { esporte } = this.state;
+        const { esport } = this.state;
         return (
             <View style={styles.container}>
 
@@ -51,36 +56,45 @@ export default class Categorias extends Component {
                     backgroundColor="transparent"
                 />
 
-                <FlatList
-                    style={styles.containerFlatList}
-                    data={this.state.listData}
-                    keyExtractor={listarposts => String(listarposts._id)}
-                    renderItem={({ item }) => {
-                        return (
-                            <View
-                                style={{
-                                    alignItems: 'center',
-                                    marginTop: 10,
-                                    marginBottom: 10
-                                }}
-                            >
-                                <TouchableOpacity onPress={() => this.navegar(item.nomeEsporte)}>
-                                    <Text
-                                        style={{
-                                            fontSize: 30,
-                                            color:"#fff",
-                                        }}
-                                    >
-                                        {item.nomeEsporte}
-                                    </Text>
-                                </TouchableOpacity>
+                {esporte && (
+                    <View style={{ width: '100%' }}>
+                        <TouchableOpacity onPress={this.navegar('Futebol')} style={styles.touchableView}>
+                            <ImageBackground source={require(`../../assets/img/futebol.gif`)} style={{ width: '100%', height: '100%', }}>
+                                <Text style={styles.flatlistText}>
+                                    Futebol
+                                       </Text>
+                            </ImageBackground>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this.navegar('Basquete')} style={styles.touchableView}>
+                            <ImageBackground source={require(`../../assets/img/basquete.gif`)} style={{ width: '100%', height: '100%' }}>
+                                <Text style={styles.flatlistText}>
+                                   Basquete
+                                </Text>
+                            </ImageBackground>
+                        </TouchableOpacity>
+                    </View>
+                )}
 
-                            </View>
-                        );
-                    }}
-                />
+                {esport && (
+                    <View style={{ width: '100%' }}>
+                        <TouchableOpacity onPress={this.navegar('CS:GO')} style={styles.touchableView}>
+                            <ImageBackground source={require(`../../assets/img/coldGif.gif`)} style={{ width: '100%', height: '100%', }}>
+                                <Text style={styles.flatlistText}>
+                                    CS:GO
+                        </Text>
+                            </ImageBackground>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this.navegar('LoL')} style={styles.touchableView}>
+                            <ImageBackground source={require(`../../assets/img/giphy.webp`)} style={{ width: '100%', height: '100%' }}>
+                                <Text style={styles.flatlistText}>
+                                    League Of Legends
+                        </Text>
+                            </ImageBackground>
+                        </TouchableOpacity>
+                    </View>
+                )}
 
-            </View>
+            </View >
         );
     }
 }
--- src/pages/EscolherCategoriaBanco/styles.js
+++ src/pages/EscolherCategoriaBanco/styles.js
@@ -5,11 +5,29 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor:'#6a1b9a',
+        backgroundColor: '#6a1b9a',
     },
     containerFlatList: {
-        marginTop: '70%'
+        marginTop: '80%',
+        width: '100%'
     },
+
+    flatlistText: {
+        fontSize: 27,
+        color: "#fff",
+        fontWeight:"bold",
+        textAlign: "center",
+        paddingTop:20,
+    },
+    touchableView: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: 80,    
+
+        marginTop: 15,
+    }
 });
 
 export default styles;
+
